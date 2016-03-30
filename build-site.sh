c5e1adb802be68c9fd354c12b5baea80389717b8
Felipe Taboada <fliptaboada@gmail.com>
2016-03-30 12:38:32 -0300

    atualizando docs/adicionado script pra facilitar atualização

---
+++ build-site.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git submodule add --force git@github.com:esusab/integracao-private.git
+git submodule update
+jekyll build -s ./integracao-private/docs/
+cp -r _site/* ./
+rm -rf .git/modules/integracao-private/
+git submodule deinit --force .
+rm .gitmodules
+rm -r integracao-private/

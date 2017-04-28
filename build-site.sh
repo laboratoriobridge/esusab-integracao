eed781c4e1fa8fe222020d1f8670ba667d8960ce
Gabriel Geraldeli <ggeraldeli@gmail.com>
2017-04-28 10:14:25 -0300

    gerar na pasta 210

--- build-site.sh
+++ build-site.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 git submodule add --force git@github.com:esusab/integracao-private.git
-git submodule update
+cd .git/modules/integracao-private/
+git checkout 56b22c8bfec4879908254c37fb0813abb540a7d6
+cd ../../../
 jekyll build -s ./integracao-private/docs/
 cp -r _site/* ./v210/
 rm -rf .git/modules/integracao-private/

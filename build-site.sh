3ba1b35f71441e0b154c8f71423d33e120ccb21a
Gabriel Geraldeli <ggeraldeli@gmail.com>
2017-04-28 10:10:16 -0300

    v210

--- build-site.sh
+++ build-site.sh
@@ -2,7 +2,7 @@
 git submodule add --force git@github.com:esusab/integracao-private.git
 git submodule update
 jekyll build -s ./integracao-private/docs/
-cp -r _site/* ./
+cp -r _site/* ./v210/
 rm -rf .git/modules/integracao-private/
 git submodule deinit --force .
 rm .gitmodules

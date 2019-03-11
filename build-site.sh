cf918c4b7d9962d5279fb85c81ea353f3524c9e4
Matheus Dias <matheusdias@users.noreply.github.com>
2019-03-11 13:23:55 -0300

    Update build-site.sh

--- build-site.sh
+++ build-site.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-git submodule add --force git@github.com:esusab/integracao-private.git
+git submodule add --force git@github.com:laboratoriobridge/esusab-integracao-private.git
 git submodule update
-jekyll build -s ./integracao-private/docs/
+jekyll build -s ./esusab-integracao-private/docs/
 cp -r _site/* ./
-rm -rf .git/modules/integracao-private/
+rm -rf .git/modules/esusab-integracao-private/
 git submodule deinit --force .
 rm .gitmodules
-rm -r integracao-private/
+rm -r esusab-integracao-private/

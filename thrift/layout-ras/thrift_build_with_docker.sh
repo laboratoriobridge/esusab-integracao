02c89bff3bdea6e6b3aac0b534a97b5c1fb05e45
lucas <lucasjoao.lj@gmail.com>
2022-03-03 02:56:57 -0300

    Script para geração dos arquivos via docker

---
+++ thrift/layout-ras/thrift_build_with_docker.sh
@@ -0,0 +1,11 @@
+LANGS="java:beans php delphi csharp rb go py"
+
+for LANG in $LANGS
+do
+    for FILE in thrift/*.thrift
+    do
+        echo "Geração do .thrift $FILE para a linguagem $LANG..."
+        docker run -v "$PWD:$(pwd)" thrift:0.9.3 thrift -o $(pwd) --gen $LANG $(pwd)/$FILE
+    done
+
+done
